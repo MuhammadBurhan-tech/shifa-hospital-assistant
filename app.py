87bd115386608037fd27a6e64d6fadfd53385bb8
MuhammadBurhan-tech <burhanibrar4@gmail.com>
2026-09-15 21:44:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_community.vectorstores import FAISS
 from groq import Groq
 
 # ---- Config ----
-FAISS_INDEX_PATH = "faiss_index"
+FAISS_INDEX_PATH = "index_faiss"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 GROQ_MODEL = "openai/gpt-oss-120b"
 TOP_K = 4
